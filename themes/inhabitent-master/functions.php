7335fa2765d9f9de040f489768c9aae10a5cb7ac
hilldmc <hilldmc@gmail.com>
2020-02-24 20:47:03 -0800

    wp theme appears to have no style - need to figure out

--- themes/inhabitent-master/functions.php
+++ themes/inhabitent-master/functions.php
@@ -83,8 +83,9 @@ add_filter( 'stylesheet_uri', 'inhabitent_starter_minified_css', 10, 2 );
  * Enqueue scripts and styles.
  */
 function inhabitent_starter_scripts() {
-	wp_enqueue_style( 'inhabitent-starter-style', get_stylesheet_uri() );
+	wp_enqueue_style( 'inhabitent-starter-style', get_stylesheet_directory_uri() . 'themes/inhabitent-master/build/css/style.min.css');
 	wp_enqueue_style('inhabitent-starter-font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css');
+
 
 	wp_enqueue_script( 'inhabitent-starter-navigation', get_template_directory_uri() . '/build/js/navigation.min.js', array(), '20151215', true );
 	wp_enqueue_script( 'inhabitent-starter-skip-link-focus-fix', get_template_directory_uri() . '/build/js/skip-link-focus-fix.min.js', array(), '20151215', true );
