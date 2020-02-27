f3ece16cae6bd83781e4ca361ac5e8ed93ecc627
hilldmc <hilldmc@gmail.com>
2020-02-26 23:03:24 -0800

    footer has background image

--- themes/inhabitent-master/footer.php
+++ themes/inhabitent-master/footer.php
@@ -10,8 +10,12 @@
 			</div><!-- #content -->
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
+				<div class="contact-info">
+					<h3>Contact Info</h3>
+					<p>info@inhabitent.com</p>
+					<p>778-456-7891</p>
 				<div class="site-info">
-					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'Hillary' ); ?></a>
+					<!-- <a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'Hillary' ); ?></a> -->
 				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
