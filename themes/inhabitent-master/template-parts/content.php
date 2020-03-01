2fd3393a2ba35b9f994224381e935415b46fb27b
hilldmc <hilldmc@gmail.com>
2020-03-01 01:26:04 -0800

    figured out how to set blog img as background

--- themes/inhabitent-master/template-parts/content.php
+++ themes/inhabitent-master/template-parts/content.php
@@ -7,28 +7,34 @@
 
 ?>
 
+
 <article class="journal-box" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-	<header class="entry-header journal-header">
-		<?php if ( has_post_thumbnail() ) : ?>
-			<div class="journal-img-box">
-			<img class="journal-img"><?php the_post_thumbnail(); ?>
-		<?php endif; ?>
-		</div>
+	<?php if ( has_post_thumbnail() ) {
+		$journalImg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), "full");
+		}  
+	?>
+			
+		<div class="journal-img" style="background: url('<?php echo $journalImg[0]; ?>') no-repeat; background-size: cover;">
+	
+				<div class="journal-title">
+						<a href="<?php the_permalink()?>" <?php the_title(); ?>>
+						<h2><?php the_title() ?><h2>
+						</a>
+				</div>
 
-		<div class="journal-title">
-		<h2><?php the_title() ?><h2>
-		</div>
+				<?php if ( 'post' === get_post_type() ) : ?>
+				<div class="entry-meta journal-meta">
+					<h4><?php inhabitent_starter_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?> / <?php inhabitent_starter_posted_by(); ?>
+					</h4>
+				</div><!-- .entry-meta -->
+				<?php endif; ?>
+		</div> 
 
-		<?php if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php inhabitent_starter_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?> / <?php inhabitent_starter_posted_by(); ?>
-		</div><!-- .entry-meta -->
-		<?php endif; ?>
-	</header><!-- .entry-header -->
 
 	<div class="entry-content">
 		<?php the_excerpt(); ?>
 	</div><!-- .entry-content -->
 
 </article><!-- #post-## -->
+
