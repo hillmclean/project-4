f1e9177a503957ab7adf37e49d778f2c2045cf06
hilldmc <hilldmc@gmail.com>
2020-02-29 21:34:20 -0800

    tax page filters category but not each item - need to fix

--- themes/inhabitent-master/taxonomy-product_taxonomy.php
+++ themes/inhabitent-master/taxonomy-product_taxonomy.php
@@ -17,11 +17,41 @@ get_header(); ?>
 				?>
 			</header><!-- .page-header -->
 
+	
+	<?php $terms = get_terms( array(
+		'taxonomy'=>'product_taxonomy',
+		'hide_empty' => 0,
+		));
 
-					
+		if (! empty($terms) && ! is_wp_error($terms)) :
+			?>
+		
+
+	<div class="product-grid">
+		<?php foreach($terms as $term): setup_postdata ($term); ?>
+			 
+		<div class="product-item">
+			<?php if( get_field('image') ): ?>
+				<a href="<?php the_permalink()?>" <?php the_title(); ?>>
+					<div class="product-img-box" style="background: url('<?php echo the_field('image'); ?>') no-repeat; background-size: cover;" >
+			<?php endif; ?>	
+					</div> 
+				</a>
+
+			<div class="product-info-box">
+				<p><?php the_title(); ?></p>
+				<p>$ <?php the_field('price'); ?></p>
+			</div> 
+		</div> <!-- .product-item -->
+
+		<?php endforeach; ?>
+		<?php endif; ?>
+		<?php wp_reset_postdata(); ?>
+
+	
+	</div> <!-- .product-grid -->
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
