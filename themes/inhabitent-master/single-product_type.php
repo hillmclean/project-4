a7b7680a78ca89244ba36d0c1e1825d95fc15356
hilldmc <hilldmc@gmail.com>
2020-02-29 22:01:58 -0800

    grid set for single product type page

--- themes/inhabitent-master/single-product_type.php
+++ themes/inhabitent-master/single-product_type.php
@@ -14,25 +14,9 @@ get_header(); ?>
 
 			<?php get_template_part( 'template-parts/content', 'single-product_type' ); ?>
 
-			<!-- <?php the_post_navigation(); ?> -->
-
-			<?php
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
-			?>
-
-		<?php endwhile; // End of the loop. ?>
-
-	
-
-		<?php if( get_field('image') ): ?>
-    		<img src="<?php the_field('image'); ?>" />
-		<?php endif; ?>
+			<?php endwhile; // End of the loop. ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
--- themes/inhabitent-master/template-parts/content-single-product_type.php
+++ themes/inhabitent-master/template-parts/content-single-product_type.php
@@ -8,18 +8,21 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
+<div class="shop-item-container">
+
+		<?php if( get_field('image') ): ?>
+			<div class="single-product-img">
+				<img src="<?php the_field('image'); ?>" />
+			</div>
+		<?php endif; ?>
+
 		<?php if ( has_post_thumbnail() ) : ?>
 			<?php the_post_thumbnail( 'large' ); ?>
 		<?php endif; ?>
 
+		<div class="entry-content">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 		<p>$ <?php the_field('price'); ?></p>
-
-
-	</header><!-- .entry-header -->
-
-	<div class="entry-content">
 		<?php the_content(); ?>
 		<?php
 			wp_link_pages( array(
@@ -27,9 +30,16 @@
 				'after'  => '</div>',
 			) );
 		?>
+
+				<?php
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif; ?>
 	</div><!-- .entry-content -->
 
 	<footer class="entry-footer">
 		<?php inhabitent_starter_entry_footer(); ?>
 	</footer><!-- .entry-footer -->
+			</div>
 </article><!-- #post-## -->
