2c030afd9f6a459bb3bd88c3c16dcf29ee1300bb
hilldmc <hilldmc@gmail.com>
2020-03-01 00:27:37 -0800

    journal box styling starting to work

--- themes/inhabitent-master/template-parts/content-single-product_type.php
+++ themes/inhabitent-master/template-parts/content-single-product_type.php
@@ -15,9 +15,7 @@
 		<?php endif; ?>
 		</div>
 
-		<?php if ( has_post_thumbnail() ) : ?>
-			<?php the_post_thumbnail( 'large' ); ?>
-		<?php endif; ?>
+	
 
 		<div class="entry-content">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
--- themes/inhabitent-master/template-parts/content.php
+++ themes/inhabitent-master/template-parts/content.php
@@ -7,13 +7,18 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php if ( has_post_thumbnail() ) : ?>
-			<?php the_post_thumbnail( 'large' ); ?>
-		<?php endif; ?>
+<article class="journal-box" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+	<header class="entry-header journal-header">
+		<?php if ( has_post_thumbnail() ) : ?>
+			<div class="journal-img-box">
+			<img class="journal-img"><?php the_post_thumbnail(); ?>
+		<?php endif; ?>
+		</div>
+
+		<div class="journal-title">
+		<h2><?php the_title() ?><h2>
+		</div>
 
 		<?php if ( 'post' === get_post_type() ) : ?>
 		<div class="entry-meta">
@@ -25,4 +30,5 @@
 	<div class="entry-content">
 		<?php the_excerpt(); ?>
 	</div><!-- .entry-content -->
+
 </article><!-- #post-## -->
