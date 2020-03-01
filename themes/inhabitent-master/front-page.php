d198cc53514a2e1a6625878fadc92fa2b1edca09
hilldmc <hilldmc@gmail.com>
2020-03-01 15:01:54 -0800

    journal boxes on front have bkgd img and in grid but not done

--- themes/inhabitent-master/front-page.php
+++ themes/inhabitent-master/front-page.php
@@ -21,6 +21,7 @@ if (! empty($terms) && ! is_wp_error($terms)) :
 
 
 	<div class="product-type-blocks">
+
 		<?php foreach($terms as $term) : ?>
 			<div class="product-type-box">
 				<img class="product-type-img" src="<?php echo get_template_directory_uri(); ?>/product-type-icons/<?php echo $term->name?>.svg" />
@@ -28,37 +29,49 @@ if (! empty($terms) && ! is_wp_error($terms)) :
 				<button class="product-type-button"><a class="button-text" href="<?php echo get_term_link($term); ?>">
 				<?php echo $term->name ?> Stuff</a>
 				</button>
-			</div>
+			</div> 
+			<!-- product-type-box -->
 
 		<?php endforeach; ?>
 		<?php endif; ?>
 
 	</div>
+	<!-- product-type-blocks -->
 
 	<h1 class="front-h1">Inhabitent Journal</h1>
 				
-	<?php
-	$args = array( 
-		'post_type' => 'post',
-		'posts_per_page' => 3
-	);
-	$blog_posts = get_posts( $args ); 
-	?>
-		<div class="journal-container">
+		<?php
+		$args = array( 
+			'post_type' => 'post',
+			'posts_per_page' => 3
+		);
+		$blog_posts = get_posts( $args ); 
+		?>
+
+		<div class="journal-grid" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+				<?php foreach($blog_posts as $post): setup_postdata ($post); ?>
+
+					<?php if ( has_post_thumbnail() ) {
+						$journalImg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), "full");} ?>
 			
-			<?php foreach($blog_posts as $post): setup_postdata ($post); ?>
-				<div class="journal-teaser">
-					<img class="journal-teaser-img"><?php the_post_thumbnail(); ?>
+			<div class="journal-teaser">
+					<div class="journal-img" style="background: url('<?php echo $journalImg[0]; ?>') no-repeat; background-size: cover;">
+					</div>
+				
 					<div class="journal-teaser-info">
 						<p><?php inhabitent_starter_posted_on(); ?> / <?php comments_number(); ?></p>
-						<h2><?php the_title(); ?></h2>
-						<button class="journal-button"><p class="button-text">Read Entry</p></button>
+							<a href="<?php the_permalink()?>">
+							<h2><?php the_title() ?></h2>
+							</a>
+						<button class="journal-button"><a class="button-text" href="<?php the_permalink()?>">Read Entry</a>
+						</button>
 					</div>
-				</div>
-
+			</div>
+	
 			<?php endforeach; ?>
 			<?php  wp_reset_postdata(); ?>
-		</div>
+		</div> 	<!-- journal-grid -->
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
